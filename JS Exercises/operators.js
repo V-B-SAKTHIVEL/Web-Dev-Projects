2aab8fb8c741f9cc7fbe1742331a52e3ac41648a
V-B-SAKTHIVEL <112497833+V-B-SAKTHIVEL@users.noreply.github.com>
2024-08-18 08:03:17 +0530

    Create operators.js

---
+++ JS Exercises/operators.js
@@ -0,0 +1,29 @@
+//Arithmetic Operators 
+console.log(3 + 2); 
+console.log(3 - 2); 
+console.log(3 * 2); 
+console.log(6 / 3); 
+console.log(6 % 3);
+
+//Increment and Decrement
+let count = 0;
+console.log(count++);
+console.log(++count); // there's a diff bw them that's why I wrote them separately
+
+console.log(count--);
+console.log(--count);
+
+//Assignment Operators
+console.log(count += 3);
+console.log(count -= 2);
+console.log(count *= 6);
+console.log(count /= 3);
+console.log(count %= 2);
+
+//Comparison Operators
+console.log("JavaScript" === "Language"); //false - === compares values and types. types are same but not values.
+console.log("JavaScript" == "JavaScript"); //true - == compares values. here values are the same.
+console.log(6 > 5); //true
+console.log(6 < 5); //false
+console.log(5 <= 5); //true
+console.log(4 >= 6); //false
